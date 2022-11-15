0d1f5fade028ef0c39c9043315923b1fe1a3cca6
Florin Buffet <73933252+FlorinBuffet@users.noreply.github.com>
2022-11-15 22:23:52 +0100

    Commented change_password.php

--- General/change_password.php
+++ General/change_password.php
@@ -1,3 +1,3 @@
-$user_id = get_user_by('login', 'your_username')->ID;
-$new_password = 'your_new_password'
-wp_set_password( $new_password, $user_id )
+$user_id = get_user_by('login', 'your_username')->ID; // replace 'your_username' by username you want to change password for
+$new_password = 'your_new_password'; // replace 'your_new_password' by the new password you want to set for 'your_username'
+wp_set_password( $new_password, $user_id );
